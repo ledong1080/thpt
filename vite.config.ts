f6a011833b291250b54f8c26b30fa8b1433a10f1
ledong1080 <67459708+ledong1080@users.noreply.github.com>
2025-10-24 14:13:33 +0700

    Thêm workflow GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '')
+  const env = loadEnv(mode, '.', '');
+
   return {
-    base: '/thpt/', // ⚠️ tên repo của bạn
+    base: '/thpt/', // 👈 phải trùng với tên repo của bạn
+
     server: {
       port: 3000,
-      host: '0.0.0.0'
+      host: '0.0.0.0',
     },
     plugins: [react()],
     define: {
@@ -17,8 +19,8 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.')
-      }
-    }
-  }
-})
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+  };
+});
